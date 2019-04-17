43fe70208c2cb0d2614e8dca9665491770028f2a
Anton Bukov <bukov.anton@gmail.com>
2019-04-17 23:57:09 +0300

    lab1

--- solutions/buk/lab1/kern/monitor.c
+++ solutions/buk/lab1/kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display backtrace", mon_backtrace },
 };
 #define NCOMMANDS (sizeof(commands)/sizeof(commands[0]))
 
@@ -58,7 +59,20 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	// Your code here.
+    uint32_t ebp = read_ebp();
+    uint32_t * ebp_p = (uint32_t*)ebp;
+
+    cprintf("Stack backtrace:\n");
+
+    while(ebp_p != 0) {
+        cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n", ebp_p, *(ebp_p + 1), *(ebp_p + 2), *(ebp_p + 3), *(ebp_p + 4), *(ebp_p + 5));
+        struct Eipdebuginfo info;
+        debuginfo_eip(*(ebp_p + 1), &info);
+        cprintf("         %s:%d: ", info.eip_file, info.eip_line);
+        cprintf("%.*s", info.eip_fn_namelen, info.eip_fn_name);
+        cprintf("+%d\n", *(ebp_p + 1) - info.eip_fn_addr);
+        ebp_p = (uint32_t*)(*ebp_p);
+    }
 	return 0;
 }
 
