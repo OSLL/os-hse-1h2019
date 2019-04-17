2415afcf5ebe6a17de37eb71389b0e470c3f3d50
vvselischev <800-99@mail.ru>
2019-04-17 18:04:53 +0000

    Lab1 done be Selishchev

--- solutions/sel/lab1/kern/monitor.c
+++ solutions/sel/lab1/kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "mon_backtrace", "Prints the stack of calls", mon_backtrace },
 };
 #define NCOMMANDS (sizeof(commands)/sizeof(commands[0]))
 
@@ -58,7 +59,27 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
-	// Your code here.
+	uint32_t *ebp = (uint32_t *)read_ebp();
+	uint32_t *eip;
+	uint32_t args[5], i;
+	struct Eipdebuginfo info;
+	
+	cprintf("Stack backtrace:\n");
+	while (ebp != NULL)
+	{ 
+		eip = (uint32_t *)ebp[1];
+		for (i = 0; i < 5; i++)
+		{
+			args[i] = ebp[i + 2];
+		}
+
+		debuginfo_eip((uintptr_t)eip, &info);		
+
+		cprintf("  ebp %08x eip %08x args %08x %08x %08x %08x %08x\n", ebp, eip, args[0], args[1], args[2], args[3], args[4]);
+		cprintf("    %s:%d: %.*s+%d\n", info.eip_file, info.eip_line, info.eip_fn_namelen, info.eip_fn_name, ebp[1] - info.eip_fn_addr);		
+
+		ebp = (uint32_t *)ebp[0];
+	}
 	return 0;
 }
 
