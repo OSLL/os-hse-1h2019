7d7bcd085c6576b3b11f795c233e047737dafcde
krock21rus <krock21rus@gmail.com>
2019-05-10 20:34:03 +0300

    Revert "lab1 done"
    This reverts commit af8e94e36d92486ab9157afcb11b6545b4b04aeb.

--- solutions/gae/lab1/lib/printfmt.c
+++ solutions/gae/lab1/lib/printfmt.c
@@ -205,10 +205,11 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 
 		// (unsigned) octal
 		case 'o':
-            //putch('0', putdat);
-		    num = getuint(&ap, lflag);
-		    base = 8;
-		    goto number;
+			// Replace this with your code.
+			putch('X', putdat);
+			putch('X', putdat);
+			putch('X', putdat);
+			break;
 
 		// pointer
 		case 'p':
