b5c120197d60d2b4ba671f91bcdf4ccf43ec6ef2
Maria Palchenkova <mashapalchenkova@gmail.com>
2019-05-19 22:26:09 +0300

    add lib/ changes

--- solutions/pal/lab3/lib/printfmt.c
+++ solutions/pal/lab3/lib/printfmt.c
@@ -206,9 +206,12 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 		// (unsigned) octal
 		case 'o':
 			// Replace this with your code.
-			putch('X', putdat);
-			putch('X', putdat);
-			putch('X', putdat);
+			//putch('X', putdat);
+			//putch('X', putdat);
+			//putch('X', putdat);
+			num = getint(&ap, lflag);
+			base = 8;
+			goto number;
 			break;
 
 		// pointer
