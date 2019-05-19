3f0ac3749a63565ded34bce0e52c2832fddf56f7
Maria Palchenkova <mashapalchenkova@gmail.com>
2019-05-19 22:32:02 +0300

    add kern/ changes

--- solutions/pal/lab3/kern/syscall.c
+++ solutions/pal/lab3/kern/syscall.c
@@ -21,6 +21,7 @@ sys_cputs(const char *s, size_t len)
 	// Destroy the environment if not.
 
 	// LAB 3: Your code here.
+	user_mem_assert(curenv, s, len, PTE_U);
 
 	// Print the string supplied by the user.
 	cprintf("%.*s", len, s);
@@ -70,6 +71,20 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 	// Return any appropriate return value.
 	// LAB 3: Your code here.
 
-	panic("syscall not implemented");
+//	panic("syscall not implemented");
+	if (syscallno == SYS_cputs) {
+		sys_cputs((const char*) a1, a2);
+		return 0;
+	}
+	if (syscallno == SYS_cgetc) {
+		return sys_cgetc();
+	}
+	if (syscallno == SYS_getenvid) {
+		return sys_getenvid();
+	}
+	if (syscallno == SYS_env_destroy) {
+		return sys_env_destroy((envid_t)a1);
+	}
+	return -E_INVAL;
 }
 
